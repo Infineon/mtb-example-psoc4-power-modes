4cbb88fff7b04a184a78e87ece8d473c8dbfb347
gitlab-runner <do-not-reply@cypress.com>
2021-10-14 17:31:44 -0500

    Upload mtb-example-psoc4-power-modes 1.1.0.276

--- main.c
+++ main.c
@@ -1,43 +1,45 @@
 /*******************************************************************************
- * File Name:   main.c
- *
- * Description:
- *  This example demonstrates how to transition PSoC 4 MCU among the following
- *  low power modes: Sleep and Deep Sleep.
- *
- * Related Document: See readme.md
- *
- *******************************************************************************
- * (c) 2020, Cypress Semiconductor Corporation. All rights reserved.
- *******************************************************************************
- * This software, including source code, documentation and related materials
- * ("Software"), is owned by Cypress Semiconductor Corporation or one of its
- * subsidiaries ("Cypress") and is protected by and subject to worldwide patent
- * protection (United States and foreign), United States copyright laws and
- * international treaty provisions. Therefore, you may use this Software only
- * as provided in the license agreement accompanying the software package from
- * which you obtained this Software ("EULA").
- *
- * If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
- * non-transferable license to copy, modify, and compile the Software source
- * code solely for use in connection with Cypress's integrated circuit products.
- * Any reproduction, modification, translation, compilation, or representation
- * of this Software except as specified above is prohibited without the express
- * written permission of Cypress.
- *
- * Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
- * reserves the right to make changes to the Software without notice. Cypress
- * does not assume any liability arising out of the application or use of the
- * Software or any product or circuit described in the Software. Cypress does
- * not authorize its products for use in any products where a malfunction or
- * failure of the Cypress product may reasonably be expected to result in
- * significant property damage, injury or death ("High Risk Product"). By
- * including Cypress's product in a High Risk Product, the manufacturer of such
- * system or application assumes all risk of such use and in doing so agrees to
- * indemnify Cypress against all liability.
- ******************************************************************************/
+* File Name:   main.c
+*
+* Description:
+*  This example demonstrates how to transition PSoC 4 MCU among the following
+*  low power modes: Sleep and Deep Sleep.
+*
+* Related Document: See readme.md
+*
+*
+*******************************************************************************
+* Copyright 2020-2021, Cypress Semiconductor Corporation (an Infineon company) or
+* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
+*
+* This software, including source code, documentation and related
+* materials ("Software") is owned by Cypress Semiconductor Corporation
+* or one of its affiliates ("Cypress") and is protected by and subject to
+* worldwide patent protection (United States and foreign),
+* United States copyright laws and international treaty provisions.
+* Therefore, you may use this Software only as provided in the license
+* agreement accompanying the software package from which you
+* obtained this Software ("EULA").
+* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
+* non-transferable license to copy, modify, and compile the Software
+* source code solely for use in connection with Cypress's
+* integrated circuit products.  Any reproduction, modification, translation,
+* compilation, or representation of this Software except as specified
+* above is prohibited without the express written permission of Cypress.
+*
+* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
+* reserves the right to make changes to the Software without notice. Cypress
+* does not assume any liability arising out of the application or use of the
+* Software or any product or circuit described in the Software. Cypress does
+* not authorize its products for use in any products where a malfunction or
+* failure of the Cypress product may reasonably be expected to result in
+* significant property damage, injury or death ("High Risk Product"). By
+* including Cypress's product in a High Risk Product, the manufacturer
+* of such system or application assumes all risk of such use and in doing
+* so agrees to indemnify Cypress against all liability.
+*******************************************************************************/
 
 /*******************************************************************************
  * Include header files
@@ -47,11 +49,11 @@
 #include "cycfg_pins.h"
 
 
-/******************************************************************************
+/*******************************************************************************
  * Macros
- *****************************************************************************/
-#define LED_ON                  (1U)
-#define LED_OFF                 (0U)
+ ******************************************************************************/
+#define LED_DIM                 (10U)    /* 10% Duty cycyle */
+#define LED_ON                  (100U)   /* 100% Duty cycle */
 #define SWITCH_INTR_PRIORITY    (3U)
 
 /* Constants to define SHORT and LONG presses on User Button (x10 = ms) */
@@ -59,22 +61,26 @@
 #define LONG_PRESS_COUNT        (200U)   /* button pressed time > 2 sec */
 
 /* Delays */
-#define DELAY                   (10U)
-#define BLINK_TIME_MS           (200U)
+#define DELAY_MS                (10U)    /* in ms */
+#define LED_DIM_CONTROL         (100U)   /* in ms */
 
 /* Glitch delays */
-#define SHORT_GLITCH_DELAY_MS   (20U)     /* in ms */
-#define LONG_GLITCH_DELAY_MS    (250U)    /* in ms */
+#define SHORT_GLITCH_DELAY_MS   (20U)    /* in ms */
+#define LONG_GLITCH_DELAY_MS    (250U)   /* in ms */
 
+/* Changes the brightness of the LED by changing the duty cycle */
+#define PWM_LED_DIM(x)        Cy_TCPWM_PWM_SetPeriod0(USER_PWM_HW, USER_PWM_NUM, LED_DIM_CONTROL); \
+                            Cy_TCPWM_PWM_SetCompare0(USER_PWM_HW, USER_PWM_NUM, x); \
+                            Cy_TCPWM_PWM_SetCounter(USER_PWM_HW, USER_PWM_NUM, 0);
 
 /******************************************************************************
  * Enumerated data types
  *****************************************************************************/
 typedef enum
 {
-    SWITCH_NO_EVENT     = 0U,
-    SWITCH_SHORT_PRESS  = 1U,
-    SWITCH_LONG_PRESS   = 2U,
+    SWITCH_NO_EVENT,
+    SWITCH_SHORT_PRESS,
+    SWITCH_LONG_PRESS,
 } en_switch_event_t;
 
 
@@ -87,8 +93,7 @@ cy_stc_scb_uart_context_t CYBSP_UART_context;
 /*******************************************************************************
  * Function Prototypes
  ******************************************************************************/
-void switch_isr();
-void led_blink(uint32_t blink_time, uint32_t num_toggles);
+void switch_isr(void);
 en_switch_event_t get_switch_event(void);
 
 /* Sleep Callback function */
@@ -109,20 +114,20 @@ const cy_stc_sysint_t switch_intr_config = {
 
 
 /*******************************************************************************
- * Function Name: main
- *******************************************************************************
- *
- * Summary:
- *  System entrance point. This function configures and initializes the GPIO
- *  interrupt, UART Component and Register callback functions.
- *
- * Parameters:
- *  void
- *
- * Return:
- *  int
- *
- ******************************************************************************/
+* Function Name: main
+********************************************************************************
+*
+* Summary:
+* System entrance point. This function configures and initializes the GPIO
+* interrupt, PWM Component, UART Component and Register callback functions.
+*
+* Parameters:
+*  void
+*
+* Return:
+*  int
+*
+*******************************************************************************/
 int main(void)
 {
     cy_rslt_t result;
@@ -136,6 +141,18 @@ int main(void)
 
     /* Enable global interrupts */
     __enable_irq();
+
+    /* Initialize PWM using the config structure generated using device configurator*/
+    if (CY_TCPWM_SUCCESS != Cy_TCPWM_PWM_Init(USER_PWM_HW, USER_PWM_NUM, &USER_PWM_config))
+    {
+        CY_ASSERT(0);
+    }
+
+    /* Enable the initialized PWM */
+    Cy_TCPWM_PWM_Enable(USER_PWM_HW, USER_PWM_NUM);
+
+    /* Start the PWM */
+    Cy_TCPWM_TriggerStart(USER_PWM_HW, USER_PWM_MASK);
 
     /* Configure and enable the UART peripheral */
     Cy_SCB_UART_Init(CYBSP_UART_HW, &CYBSP_UART_config, &CYBSP_UART_context);
@@ -164,6 +181,28 @@ int main(void)
             /*.context    =*/ NULL
     };
 
+    /* SysClk context structure */
+    cy_stc_sysclk_context_t sysClkContext;
+
+    /* SysClk callback params */
+    cy_stc_syspm_callback_params_t sysClkCallbackParams =
+    {
+        .base       = NULL,
+        .context    = (void*)&sysClkContext
+    };
+
+    /* Callback declaration for SysClk Deep Sleep mode */
+    cy_stc_syspm_callback_t sysClkCallback =
+    {
+        .callback       = &Cy_SysClk_DeepSleepCallback,
+        .type           = CY_SYSPM_DEEPSLEEP,
+        .skipMode       = 0UL,
+        .callbackParams = &sysClkCallbackParams,
+        .prevItm        = NULL,
+        .nextItm        = NULL,
+        .order          = 0
+    };
+
     /* Callback declaration for Sleep mode */
     cy_stc_syspm_callback_t sleep_cb    =  {sleep_callback,              /* Callback function */
                                            CY_SYSPM_SLEEP,               /* Callback type */
@@ -178,17 +217,21 @@ int main(void)
                                            &callbackParams,              /* Callback params */
                                            NULL, NULL};                  /* For internal usage */
 
+
+    /* Register SysClk Deep Sleep callback */
+    Cy_SysPm_RegisterCallback(&sysClkCallback);
+
     /* Register Sleep callback */
     Cy_SysPm_RegisterCallback(&sleep_cb);
 
     /* Register Deep Sleep callback */
     Cy_SysPm_RegisterCallback(&deep_sleep_cb);
 
+    /* Turn ON LED */
+    PWM_LED_DIM(LED_ON);
+
     for (;;)
     {
-        /* Turn ON LED */
-        Cy_GPIO_Write(CYBSP_USER_LED1_PORT, CYBSP_USER_LED1_NUM, LED_ON);
-
         switch (get_switch_event())
         {
             case SWITCH_SHORT_PRESS:
@@ -223,20 +266,20 @@ int main(void)
 
 
 /*******************************************************************************
- * Function Name: switch_isr
- *******************************************************************************
- *
- * Summary:
- *  This function is executed when GPIO interrupt is triggered.
- *  It Clears the triggered pin interrupt and Clears Pending Interrupt.
- *
- * Parameters:
- *  None
- *
- * Return:
- *  void
- *
- ******************************************************************************/
+* Function Name: switch_isr
+********************************************************************************
+*
+* Summary:
+* This function is executed when GPIO interrupt is triggered.
+* It Clears the triggered pin interrupt and Clears Pending Interrupt.
+*
+* Parameters:
+*  None
+*
+* Return:
+*  void
+*
+*******************************************************************************/
 void switch_isr()
 {
     /* Clears the triggered pin interrupt */
@@ -247,21 +290,21 @@ void switch_isr()
 
 
 /*******************************************************************************
- * Function Name: get_switch_event
- *******************************************************************************
- * Summary:
- *  Returns how the User button was pressed:
- *  - SWITCH_NO_EVENT: No press
- *  - SWITCH_SHORT_PRESS: Short press was detected
- *  - SWITCH_LONG_PRESS: Long press was detected
- *
- * Parameters:
- *  void
- *
- * Return:
- *  Switch event that occurred, if any.
- *
- ******************************************************************************/
+* Function Name: get_switch_event
+********************************************************************************
+* Summary:
+* Returns how the User button was pressed:
+*  - SWITCH_NO_EVENT: No press
+*  - SWITCH_SHORT_PRESS: Short press was detected
+*  - SWITCH_LONG_PRESS: Long press was detected
+*
+* Parameters:
+*  void
+*
+* Return:
+*  Switch event that occurred, if any.
+*
+*******************************************************************************/
 en_switch_event_t get_switch_event(void)
 {
     en_switch_event_t event = SWITCH_NO_EVENT;
@@ -271,7 +314,7 @@ en_switch_event_t get_switch_event(void)
     while (Cy_GPIO_Read(CYBSP_USER_BTN_PORT, CYBSP_USER_BTN_NUM) == CYBSP_BTN_PRESSED)
     {
         /* Wait for 10 ms */
-        Cy_SysLib_Delay(DELAY);
+        Cy_SysLib_Delay(DELAY_MS);
 
         /* Increment counter. Each count represents 10 ms */
         press_count++;
@@ -295,21 +338,22 @@ en_switch_event_t get_switch_event(void)
 
 
 /*******************************************************************************
- * Function Name: sleep_callback
- *******************************************************************************
- *
- * Sleep callback implementation. It turns the LED off before going to
- * sleep power mode. After waking up, it sets the LED to blink for two times.
- *
- * Parameters:
- *  callbackParams: The pointer to the callback parameters structure
- *  cy_stc_syspm_callback_params_t.
- *  mode: Callback mode, see cy_en_syspm_callback_mode_t
- *
- * Return:
- *  Entered status, see cy_en_syspm_status_t.
- *
- ******************************************************************************/
+* Function Name: sleep_callback
+********************************************************************************
+*
+* Summary:
+* Sleep callback implementation. It dims the LED to 10 % duty cycle before
+* entering sleep power mode. After waking up, LED turns ON (100%).
+*
+* Parameters:
+*  callbackParams: The pointer to the callback parameters structure
+*  cy_stc_syspm_callback_params_t.
+*  mode: Callback mode, see cy_en_syspm_callback_mode_t
+*
+* Return:
+*  Entered status, see cy_en_syspm_status_t.
+*
+*******************************************************************************/
 cy_en_syspm_status_t sleep_callback(
         cy_stc_syspm_callback_params_t *callbackParams, cy_en_syspm_callback_mode_t mode)
 {
@@ -343,8 +387,8 @@ cy_en_syspm_status_t sleep_callback(
 
         case CY_SYSPM_BEFORE_TRANSITION:
 
-            /* Blink the LED for two times before entering into Sleep mode */
-            led_blink(BLINK_TIME_MS, 2);
+            /* Before going to sleep mode, dim the LED (10%) */
+            PWM_LED_DIM(LED_DIM);
 
             ret_val = CY_SYSPM_SUCCESS;
             break;
@@ -355,6 +399,9 @@ cy_en_syspm_status_t sleep_callback(
             Cy_SCB_UART_Enable(CYBSP_UART_HW);
             /* Send a string over serial terminal */
             Cy_SCB_UART_PutString(CYBSP_UART_HW, "Wake-up from Sleep mode and enters Active mode\r\n");
+
+            /* Turn ON LED */
+            PWM_LED_DIM(LED_ON);
 
             ret_val = CY_SYSPM_SUCCESS;
             break;
@@ -369,21 +416,21 @@ cy_en_syspm_status_t sleep_callback(
 
 
 /*******************************************************************************
- * Function Name: deep_sleep_callback
- *******************************************************************************
- *
- * Summary:
- *  Deep Sleep callback implementation. It turns the LED off before going to deep
- *  sleep power mode. After waking up, it sets the LED to blink for three times.
- *
- * Parameters:
- *  callbackParams: The pointer to the callback parameters structure cy_stc_syspm_callback_params_t.
- *  mode: Callback mode, see cy_en_syspm_callback_mode_t
- *
- * Return:
- *  Entered status, see cy_en_syspm_status_t.
- *
- ******************************************************************************/
+* Function Name: deep_sleep_callback
+********************************************************************************
+*
+* Summary:
+* Deep Sleep callback implementation. It turns the LED off before going to deep
+* sleep power mode. After waking up, LED turns ON (100%).
+*
+* Parameters:
+*  callbackParams: The pointer to the callback parameters structure cy_stc_syspm_callback_params_t.
+*  mode: Callback mode, see cy_en_syspm_callback_mode_t
+*
+* Return:
+*  Entered status, see cy_en_syspm_status_t.
+*
+*******************************************************************************/
 cy_en_syspm_status_t deep_sleep_callback(
         cy_stc_syspm_callback_params_t *callbackParams, cy_en_syspm_callback_mode_t mode)
 {
@@ -417,18 +464,25 @@ cy_en_syspm_status_t deep_sleep_callback(
 
         case CY_SYSPM_BEFORE_TRANSITION:
 
-            /* Blink the LED for three times before entering into Deep Sleep mode */
-            led_blink(BLINK_TIME_MS, 3);
+            /* Disable the PWM */
+            Cy_TCPWM_PWM_Disable(USER_PWM_HW, USER_PWM_NUM);
 
             ret_val = CY_SYSPM_SUCCESS;
             break;
 
         case CY_SYSPM_AFTER_TRANSITION:
 
+            /* Enable and Start the PWM */
+            Cy_TCPWM_PWM_Enable(USER_PWM_HW, USER_PWM_NUM);
+            Cy_TCPWM_TriggerStart(USER_PWM_HW, USER_PWM_MASK);
+
             /* Enable the UART */
             Cy_SCB_UART_Enable(CYBSP_UART_HW);
             /* Send a string over serial terminal */
             Cy_SCB_UART_PutString(CYBSP_UART_HW, "Wake-up from Deep Sleep mode and enters Active mode\r\n");
+
+            /* Turn ON LED */
+            PWM_LED_DIM(LED_ON);
 
             ret_val = CY_SYSPM_SUCCESS;
             break;
@@ -439,40 +493,6 @@ cy_en_syspm_status_t deep_sleep_callback(
             break;
     }
     return ret_val;
-}
-
-
-/*******************************************************************************
- * Function Name: led_blink
- *******************************************************************************
- * Summary:
- * Blinks the LED for num_toggles times, with a period of blink_time.
- *
- * Parameters:
- * blink_time:  Time in ms for on and off times.
- * num_toggles: Describes how many times to toggle the LED on and off.
- *
- * Return:
- *  void
- *
- ******************************************************************************/
-void led_blink(uint32_t blink_time, uint32_t num_toggles)
-{
-    /* variable used to set LED blink time */
-    uint8_t count = 0U;
-
-    /* Turn OFF LED */
-    Cy_GPIO_Write(CYBSP_USER_LED1_PORT, CYBSP_USER_LED1_NUM, LED_OFF);
-    Cy_SysLib_Delay(blink_time);
-
-    /* Toggle the LED the desired number of times in this loop */
-    for (count = 0; count < num_toggles; count++)
-    {
-        Cy_GPIO_Write(CYBSP_USER_LED1_PORT, CYBSP_USER_LED1_NUM, LED_ON);
-        Cy_SysLib_Delay(blink_time);
-        Cy_GPIO_Write(CYBSP_USER_LED1_PORT, CYBSP_USER_LED1_NUM, LED_OFF);
-        Cy_SysLib_Delay(blink_time);
-    }
 }
 
 /* [] END OF FILE */
